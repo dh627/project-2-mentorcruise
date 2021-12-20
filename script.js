164151ac8ea79b83c512078a15194b22e9865493
Dan Harris <danharris627@googlemail.com>
2021-12-20 16:14:58 +0000

    challenge 1 & 2

---
+++ script.js
@@ -0,0 +1,43 @@
+// let originalText =
+//   "Some quick example text to build on the card title and make up the bulk of the cards content";
+// let replacementText =
+//   "Load pot with water and pasta, salt the water, bring water to the boil, stir to keep pasta from sticking";
+
+// document.querySelector(".read-more-button-1").addEventListener("click", () => {
+//   let text = document
+//     .querySelector(".text-1")
+//     .textContent.replace(/[\n\r]+|[\s]{2,}/g, " ")
+//     .trim();
+
+//   console.log("TEXT:");
+//   console.log(text);
+
+//   console.log("Original Text:");
+//   console.log(originalText);
+
+//   if (text === originalText) {
+//     console.log("HELLO");
+//     document.querySelector(".text-1").textContent = replacementText;
+//   } else {
+//     document.querySelector(".text-1").textContent = originalText;
+//   }
+// });
+
+// challenge 1
+document.querySelector(".read-more-button-1").addEventListener("click", () => {
+  document.querySelector(".text-1").textContent =
+    "Load pot with water and pasta, salt the water, bring water to the boil, stir to keep pasta from sticking";
+});
+
+// challenge 2
+document.querySelector(".read-more-button-2").addEventListener("click", () => {
+  if (document.querySelector(".card-image-2")) {
+    document
+      .querySelector(".card-image-2")
+      .classList.toggle("card-image-2-alt");
+  } else {
+    document
+      .querySelector(".card-image-2-alt")
+      .classList.toggle("card-image-2");
+  }
+});
